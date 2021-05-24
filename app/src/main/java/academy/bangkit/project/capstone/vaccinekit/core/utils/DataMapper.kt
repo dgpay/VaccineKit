b60d652e96aa7df36440b042a9a3a92e964196bd
hrswcksono <hariswicaksono250999@gmail.com>
2021-05-24 10:26:31 +0700

    update

---
+++ app/src/main/java/academy/bangkit/project/capstone/vaccinekit/core/utils/DataMapper.kt
@@ -0,0 +1,29 @@
+package academy.bangkit.project.capstone.vaccinekit.core.utils
+
+import academy.bangkit.project.capstone.vaccinekit.core.data.source.local.entity.VaccineEntity
+import academy.bangkit.project.capstone.vaccinekit.core.domain.model.Vaccine
+
+object DataMapper {
+    fun mapResponsesToEntities() {  }
+
+    fun mapEntitiesToDomain(input: List<VaccineEntity>): List<Vaccine> =
+        input.map {
+            Vaccine(
+                NIK = it.NIK,
+                TTL = it.TTL,
+                address = it.address,
+                date = it.date,
+                name = it.name,
+                photo = it.photo
+            )
+        }
+
+    fun mapDomainToEntity(it: Vaccine) = VaccineEntity(
+        NIK = it.NIK,
+        TTL = it.TTL,
+        address = it.address,
+        date = it.date,
+        name = it.name,
+        photo = it.photo
+    )
+}
