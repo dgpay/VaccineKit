d90ae2914d281b61ec116a23547781132c460692
dpay <deviap485@gmail.com>
2021-06-07 01:02:49 +0700

    password

--- app/src/main/java/academy/bangkit/project/capstone/vaccinekit/auth/LoginActivity.kt
+++ app/src/main/java/academy/bangkit/project/capstone/vaccinekit/auth/LoginActivity.kt
@@ -1,10 +1,14 @@
 package academy.bangkit.project.capstone.vaccinekit.auth
 
 import academy.bangkit.project.capstone.vaccinekit.MainActivity
+import academy.bangkit.project.capstone.vaccinekit.R
 import academy.bangkit.project.capstone.vaccinekit.databinding.ActivityLoginBinding
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
@@ -15,10 +19,23 @@ class LoginActivity : AppCompatActivity() {
     var mAuth = FirebaseAuth.getInstance()
     private lateinit var binding: ActivityLoginBinding
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.showHideBtn.setOnClickListener {
+            if(binding.showHideBtn.background.equals(resources.getDrawable(R.drawable.show_pass,theme))){
+                binding.pass.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                binding.showHideBtn.background = resources.getDrawable(R.drawable.show_pass,theme)
+                binding.showHideBtn.text = "show"
+            } else{
+                binding.pass.transformationMethod = PasswordTransformationMethod.getInstance()
+                binding.showHideBtn.background = resources.getDrawable(R.drawable.ic_baseline_remove_red_eye_24,theme)
+                binding.showHideBtn.text = "hide"
+            }
+        }
 
         binding.loginBtn.setOnClickListener {
             login()
